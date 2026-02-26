05e0fb08fc979327cc7e5a53f99ca19a66aba7d6
gearmindscode <126361264+gearmindscode@users.noreply.github.com>
2026-02-25 18:36:12 -0600

    Add files via upload

---
+++ chinmayi/chinmayi.py
@@ -0,0 +1,11 @@
+minon=[]
+ew=float(input("in put 3 numbers  "))
+e=float(input(" "))
+w=float(input(" "))
+s=float(input("delete one number"))
+minon.append(ew)
+minon.append(e)
+minon.append(w)
+
+
+print(minon)
