8c7fc711533f62d885f01d7d01c450873751bfe4
gearmindscode <126361264+gearmindscode@users.noreply.github.com>
2026-02-25 19:29:51 -0600

    Update test.py

--- Shreyansh/test.py
+++ Shreyansh/test.py
@@ -1,12 +1,28 @@
-list = [1, 2, 3, 4, 5, 6]
-lsst = list[:]
-print(lsst)
+list = [1, 2, 3, 4, 5, 6, 7]
+
+list_one = list[:3]
+
+list_two = list[-2:]
+
+list_three = list[:2]
+
+list_four = list[::-1]
+
+list_five = list[2:5]
+
+list_six = list[1:-1]
 
 
-lssst = [10, 8, 6, 4, 2]
-lst = lssst[1:3]
-print(lst)
+print(list_one)
 
+print(list_two)
 
-    
-    
+print(list_three)
+
+print(list_four)
+
+print(list_five)
+
+print(list_six)
+
+print(list)
