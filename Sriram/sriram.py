494b176e13b11d075a25212cf1b46059b21d8413
gearmindscode <126361264+gearmindscode@users.noreply.github.com>
2026-02-25 19:29:29 -0600

    Add files via upload

--- Sriram/sriram.py
+++ Sriram/sriram.py
@@ -1,1 +1,21 @@
-print('hello')
+p = ['chips','cookies', 'popcorn','granola bar', 'pretzels', 'fruit snacks', 'crackers']
+
+l = p[0:3]
+
+print(l)
+
+last_2 = p[-2:]
+
+print(last_2)
+
+every_other = p[::2]
+
+print(every_other)
+
+reverse = p[::-1]
+
+print(reverse)
+
+mid_3 = p[2:5]
+
+print(mid_3)
